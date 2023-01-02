f199ac3151c6b953a594c2133010250144ec1439
danieldificil <daniel.araujo@ideabox-it.com>
2023-01-02 16:56:59 -0300

    [Dia 1]

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,6 @@
 import {NegociacaoController} from "./controllers/negociacao-controller.js";
+import {Negociacoes} from "./models/negociacoes.js";
+import {Negociacao} from "./models/negociacao";
 
 const controller = new NegociacaoController()
 const form = document.querySelector('.form')
@@ -6,3 +8,4 @@ form.addEventListener('submit', (event: Event) => {
     event.preventDefault()
     controller.adiciona()
 })
+
